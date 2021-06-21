6ee058f7c1c51661b21f88fcdfe437b72ed62dc7
Aravinda HB <aru03.info@gmail.com>
2021-06-21 15:24:27 +0530

    Apps added

---
+++ appium_ws/AppiumTraining/src/main/java/com/synechron/appium/AppiumTraining/App.java
@@ -0,0 +1,13 @@
+package com.synechron.appium.AppiumTraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
